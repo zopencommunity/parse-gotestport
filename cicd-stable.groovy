015fd785403f2e07e7785f623f8dd791079b55e7
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:53 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/parse-gotestport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/parse-gotestport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/parse-gotestport.git'), string(name: 'PORT_DESCRIPTION', value: 'A tool to parse results from go test -v -json' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "v3r1" ) ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/parse-gotestport.git'), string(name: 'PORT_DESCRIPTION', value: 'A tool to parse results from go test -v -json' ), string(name: 'BUILD_LINE', value: 'STABLE'), string(name: 'NODE_LABEL', value: "v3r1" ) ]
   }
 }
